0444c99fdf4bda3edf417f43c817452de41f75c6
SBG <sblanquicett88129@umanizales.edu.co>
2022-05-25 17:59:36 -0500

    Inicion de educacion

--- src/components/education/index.js
+++ src/components/education/index.js
@@ -13,7 +13,7 @@ function education(){
           <Card.Title>EDUCACION</Card.Title>
           <Card.Text>
             Tenemos estudios terminados de primaria y bachiller.
-            La diferecia entre ambos influye en que Sayan tiene un 'Tecnico en mantenimiento de sistemas de computo' 
+            La diferecia entre ambos influye en que Sayan tiene un 'Tecnico en mantenimiento de equipos de computo' 
 
             Actualmente estamos cursando la Universidad, haciendo la carrera de 'Ingenieria de sistemas y telecomunicaciones'
           </Card.Text>
