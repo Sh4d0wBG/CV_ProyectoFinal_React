fdbad2d78fe253319e86cf35dd7197f4c97062f4
SBG <sblanquicett88129@umanizales.edu.co>
2022-05-25 17:55:28 -0500

    Modificacion about

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -15,7 +15,7 @@ function about(){
           <Card.Text>
             Somos unas personas leales, comprometidas con valor eticos, morales, alto sentido de la honestidad, pertenencia, resposabilidad, compromiso adaptacion al cambio y aprendizaje continuo.
 
-            Dispuestos a cumplir con las distintas funciones, obligaciones y normas laborales que tengan establecidas.
+            Dispuestos a cumplir con las distintas funciones, obligaciones y normas laborales que tengan establecidas
           </Card.Text>
         </Card.Body>
       </Card>
