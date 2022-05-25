81324e0a68b7def7c1e07da0807ed3cc195c606c
SBG <sblanquicett88129@umanizales.edu.co>
2022-05-25 18:13:39 -0500

    Modificacion de intereses

---
+++ src/components/intereses/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Card } from 'react-bootstrap';
+import prog from './img/prog.jpg';
+
+function interests(){
+  return(
+    <div className="card" style={{width:'40rem'}} >
+      <Card >
+        <div className="img-fluid">
+          <Card.Img variant="top" src={prog} style={{width:'20rem'}}  />
+        </div>
+        <Card.Body >
+          <Card.Title>INTERESES</Card.Title>
+          <Card.Text>
+            Ambos nos interesa bastante el tema del front, el diseño de paginas nos ha parecido interesante por lo tanto, empezamos a estudiar mas a fondo del tema ya que ahonda demasiada informacion.
+            A Sayan le interesa tambien aprender sobre la ciberseguridad o proteccion de datos.  
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+    
+    
+  )
+}
+
+export default interests;
