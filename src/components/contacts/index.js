f47f42d5d3fea5a37ce73665460691427532aa6f
SBG <sblanquicett88129@umanizales.edu.co>
2022-05-25 18:23:57 -0500

    Modificacion contact

--- src/components/contacts/index.js
+++ src/components/contacts/index.js
@@ -9,11 +9,15 @@ function contact(){
           <Card.Title>ENVIANOS TU MENSAJE!</Card.Title>
             <Form>
               <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="name@example.com" />
+                <Form.Label>Nombre Completo</Form.Label>
+                <Form.Control type="email" placeholder="" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+                <Form.Label>Correo electronico</Form.Label>
+                <Form.Control type="email" placeholder="" />
               </Form.Group>
               <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                <Form.Label>Example textarea</Form.Label>
+                <Form.Label>Mensaje</Form.Label>
                 <Form.Control as="textarea" rows={3} />
               </Form.Group>
             </Form>
